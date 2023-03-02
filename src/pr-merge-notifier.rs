4231529d568e5e2db5f123993c742b8cf7ad5679
Ji Chen <jaykchen@gmail.com>
2023-03-01 19:30:40 -0500

    issue using octocrab.get

--- src/pr-merge-notifier.rs
+++ src/pr-merge-notifier.rs
@@ -1,54 +1,66 @@
-use anyhow::{Error, Result};
 use github_flows::{get_octo, listen_to_event, EventPayload};
-use slack_flows::send_message_to_channel;
+use sendgrid_flows::{send_email, Email};
+use serde_json::Value;
 #[no_mangle]
 #[tokio::main(flavor = "current_thread")]
 pub async fn run() -> anyhow::Result<()> {
-    listen_to_event("jaykchen", "vitesse-lite", vec!["pull_request"], handler).await;
+    let your_github_login: &str = "jaykchen";
+    let your_repo_name: &str = "vitesse-lite";
+    listen_to_event(
+        your_github_login,
+        your_repo_name,
+        vec!["pull_request"],
+        handler,
+    )
+    .await;
 
     Ok(())
 }
 
-async fn get_email(user: &str) -> anyhow::Result<()> {
-    let octo = get_octo(None);
+async fn get_email(user: &String) -> String {
+    let octocrab = get_octo(None);
     let query_str = format!("/users/{user}");
 
-    let response = octo.get(query_str, None::<&()>).await?;
-    let text = format!("{:?}", response);
-    send_message_to_channel("ik8", "general", "supposedly done query".to_string());
-    send_message_to_channel("ik8", "general", text);
-    Ok(())
-    // match response.get("email") {
-    //     Some(e) => Some(e.to_string()),
-    //     None => None,
-    // }
+    let response = octocrab
+        .get(query_str, None::<&()>)
+        .await
+        .expect("no reponse");
+
+    let parsed: Value = serde_json::from_str(response).expect("failed to parse");
+
+    match parsed["email"].as_str() {
+        Some(e) => e.to_string(),
+        None => "".to_string(),
+    }
 }
 
 async fn handler(payload: EventPayload) {
-    let mut title: String = "no title found".to_string();
-    let mut merged: bool = false;
     let mut html_url: String = "no html_url found".to_string();
     let mut user: String = "".to_string();
 
     if let EventPayload::PullRequestEvent(e) = payload {
         let pr = e.pull_request;
-        title = pr.title.expect("no title");
         html_url = pr.html_url.expect("no html_url field").to_string();
         user = pr.user.expect("user not found").login;
-        merged = match pr.merge_commit_sha {
-            Some(_sha) => true,
-            None => false,
+        match pr.merge_commit_sha {
+            Some(_) => {}
+            None => {
+                return;
+            }
         };
     }
 
-    get_email(&user).await;
+    let email = get_email(&user).await;
 
-    // match get_email(&user).await {
-    //     None => {}
-    //     Some(email) => {
-    // let merged_str = if merged { "merged" } else { "null" };
-    // let text = format!("title: {title}\n html_url: {html_url}\n user: {user}\n, email: email\n merged: {merged_str}");
-    // send_message_to_channel("ik8", "general", text);
-    // }
-    // }
+    let text = format!(
+        r#"
+Hi {user}, <br/>
+Welcome to the {html_url} community, thank you for your contribution!"#
+    );
+    let email_obj = Email {
+        to: vec![email.clone()],
+        subject: String::from("Thank you for contributing to this repository"),
+        content: text,
+    };
+    send_email(&email, &email_obj).expect("failed to send email");
 }
