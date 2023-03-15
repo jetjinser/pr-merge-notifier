24384c50ea244b01315740af6f6da071dd63ba3d
Ji Chen <jaykchen@gmail.com>
2023-03-15 16:51:55 -0400

    refactored to new convention

--- src/pr-merge-notifier.rs
+++ src/pr-merge-notifier.rs
@@ -1,56 +1,54 @@
+use github_flows::octocrab::Result as OctoResult;
 use github_flows::{get_octo, listen_to_event, EventPayload};
 use sendgrid_flows::{send_email, Email};
-use serde_json::Value;
+
 #[no_mangle]
 #[tokio::main(flavor = "current_thread")]
 pub async fn run() -> anyhow::Result<()> {
-    let your_github_login: &str = "jaykchen";
-    let your_repo_name: &str = "vitesse-lite";
-    listen_to_event(
-        your_github_login,
-        your_repo_name,
-        vec!["pull_request"],
-        handler,
-    )
+    let owner: &str = "jaykchen";
+    let repo: &str = "vitesse-lite";
+
+    listen_to_event(owner, repo, vec!["pull_request"], |payload| {
+        handler(owner, payload)
+    })
     .await;
 
     Ok(())
 }
 
-async fn get_email(user: &String) -> String {
-    let octocrab = get_octo(None);
-    let query_str = format!("/users/{user}");
+async fn handler(owner: &str, payload: EventPayload) {
+    let octocrab = get_octo(Some(String::from(owner)));
 
-    let response = octocrab
-        .get(query_str, None::<&()>)
-        .await
-        .expect("no reponse");
-
-    let parsed: Value = serde_json::from_str(response).expect("failed to parse");
-
-    match parsed["email"].as_str() {
-        Some(e) => e.to_string(),
-        None => "".to_string(),
-    }
-}
-
-async fn handler(payload: EventPayload) {
-    let mut html_url: String = "no html_url found".to_string();
-    let mut user: String = "".to_string();
+    let mut user = "".to_string();
+    let mut html_url = "".to_string();
+    let mut email = "".to_string();
 
     if let EventPayload::PullRequestEvent(e) = payload {
-        let pr = e.pull_request;
-        html_url = pr.html_url.expect("no html_url field").to_string();
-        user = pr.user.expect("user not found").login;
-        match pr.merge_commit_sha {
-            Some(_) => {}
+        let pull = e.pull_request;
+        html_url = pull.html_url.unwrap().to_string();
+        user = pull.user.unwrap().login;
+
+        match pull.merge_commit_sha {
+            Some(_) => {
+                let query_str = format!("/users/{user}");
+
+                let response: OctoResult<serde_json::Value> =
+                    octocrab.get(query_str, None::<&()>).await;
+                match response {
+                    Err(_) => {}
+                    Ok(user_obj) => {
+                        email = user_obj["email"]
+                            .as_str()
+                            .unwrap_or("contributor email not found")
+                            .to_string();
+                    }
+                }
+            }
             None => {
                 return;
             }
         };
     }
-
-    let email = get_email(&user).await;
 
     let text = format!(
         r#"
