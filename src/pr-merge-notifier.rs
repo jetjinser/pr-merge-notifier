ef4d4e40d6cf64e81f64fd755129cbd15141723c
Ji Chen <jaykchen@gmail.com>
2023-03-15 17:05:49 -0400

    corrected sender_email

--- src/pr-merge-notifier.rs
+++ src/pr-merge-notifier.rs
@@ -7,21 +7,22 @@ use sendgrid_flows::{send_email, Email};
 pub async fn run() -> anyhow::Result<()> {
     let owner: &str = "jaykchen";
     let repo: &str = "vitesse-lite";
+    let sender_email_sendgrid : &str = "jaykchen@gmailcom";
 
     listen_to_event(owner, repo, vec!["pull_request"], |payload| {
-        handler(owner, payload)
+        handler(owner, sender_email_sendgrid, payload)
     })
     .await;
 
     Ok(())
 }
 
-async fn handler(owner: &str, payload: EventPayload) {
+async fn handler(owner: &str, sender_email_sendgrid: &str, payload: EventPayload) {
     let octocrab = get_octo(Some(String::from(owner)));
 
     let mut user = "".to_string();
     let mut html_url = "".to_string();
-    let mut email = "".to_string();
+    let mut contributor_email = "".to_string();
 
     if let EventPayload::PullRequestEvent(e) = payload {
         let pull = e.pull_request;
@@ -37,7 +38,7 @@ async fn handler(owner: &str, payload: EventPayload) {
                 match response {
                     Err(_) => {}
                     Ok(user_obj) => {
-                        email = user_obj["email"]
+                        contributor_email = user_obj["email"]
                             .as_str()
                             .unwrap_or("contributor email not found")
                             .to_string();
@@ -56,9 +57,9 @@ Hi {user}, <br/>
 Welcome to the {html_url} community, thank you for your contribution!"#
     );
     let email_obj = Email {
-        to: vec![email.clone()],
+        to: vec![contributor_email.to_string()],
         subject: String::from("Thank you for contributing to this repository"),
         content: text,
     };
-    send_email(&email, &email_obj).expect("failed to send email");
+    send_email(sender_email_sendgrid, &email_obj).expect("failed to send email");
 }
