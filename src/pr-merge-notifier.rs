d906bd4debf7ab9602452868c9120971a9c18fb8
Ji Chen <jaykchen@gmail.com>
2023-03-15 17:49:22 -0400

    debug getting email and url

--- src/pr-merge-notifier.rs
+++ src/pr-merge-notifier.rs
@@ -1,13 +1,16 @@
-use github_flows::octocrab::Result as OctoResult;
-use github_flows::{get_octo, listen_to_event, EventPayload};
+use github_flows::{
+    get_octo, listen_to_event,
+    octocrab::{models::events::payload::PullRequestEventAction, Result as OctoResult},
+    EventPayload,
+};
 use sendgrid_flows::{send_email, Email};
-
+use slack_flows::{send_message_to_channel};
 #[no_mangle]
 #[tokio::main(flavor = "current_thread")]
 pub async fn run() -> anyhow::Result<()> {
     let owner: &str = "jaykchen";
     let repo: &str = "vitesse-lite";
-    let sender_email_sendgrid : &str = "jaykchen@gmail.com";
+    let sender_email_sendgrid: &str = "jaykchen@gmail.com";
 
     listen_to_event(owner, repo, vec!["pull_request"], |payload| {
         handler(owner, sender_email_sendgrid, payload)
@@ -20,19 +23,22 @@ pub async fn run() -> anyhow::Result<()> {
 async fn handler(owner: &str, sender_email_sendgrid: &str, payload: EventPayload) {
     let octocrab = get_octo(Some(String::from(owner)));
 
-    let mut user = "".to_string();
+    let mut contributor = "".to_string();
     let mut html_url = "".to_string();
     let mut contributor_email = "".to_string();
 
     if let EventPayload::PullRequestEvent(e) = payload {
+        if e.action != PullRequestEventAction::Closed {
+            return;
+        }
         let pull = e.pull_request;
-        html_url = pull.html_url.unwrap().to_string();
-        user = pull.user.unwrap().login;
+        html_url = pull.html_url.expect("no html_url found").to_string();
+        contributor = pull.user.expect("no contributor info found").login;
 
         match pull.merge_commit_sha {
             Some(_) => {
-                let query_str = format!("/users/{user}");
-
+                let query_str = format!("/users/{contributor}");
+                send_message_to_channel("ik8", "general", query_str.to_string());
                 let response: OctoResult<serde_json::Value> =
                     octocrab.get(query_str, None::<&()>).await;
                 match response {
@@ -50,10 +56,11 @@ async fn handler(owner: &str, sender_email_sendgrid: &str, payload: EventPayload
             }
         };
     }
+    send_message_to_channel("ik8", "general", contributor_email.to_string());
 
     let text = format!(
         r#"
-Hi {user}, <br/>
+Hi {contributor}, <br/>
 Welcome to the {html_url} community, thank you for your contribution!"#
     );
     let email_obj = Email {
