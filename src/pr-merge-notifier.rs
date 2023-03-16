9b6f8e6b0a2438d076fed77cac6b365d89341a3a
Ji Chen <jaykchen@gmail.com>
2023-03-15 21:02:00 -0400

    direct call to the github api

--- src/pr-merge-notifier.rs
+++ src/pr-merge-notifier.rs
@@ -1,10 +1,16 @@
+use dotenv::dotenv;
 use github_flows::{
     get_octo, listen_to_event,
-    octocrab::{models::events::payload::PullRequestEventAction, Result as OctoResult},
+    octocrab::{models::events::payload::PullRequestEventAction, models::repos::GitUser},
     EventPayload,
 };
+use http_req::{
+    request::{Method, Request},
+    uri::Uri,
+};
 use sendgrid_flows::{send_email, Email};
-use slack_flows::{send_message_to_channel};
+use slack_flows::send_message_to_channel;
+use std::env;
 #[no_mangle]
 #[tokio::main(flavor = "current_thread")]
 pub async fn run() -> anyhow::Result<()> {
@@ -21,6 +27,7 @@ pub async fn run() -> anyhow::Result<()> {
 }
 
 async fn handler(owner: &str, sender_email_sendgrid: &str, payload: EventPayload) {
+    dotenv().ok();
     let octocrab = get_octo(Some(String::from(owner)));
 
     let mut contributor = "".to_string();
@@ -33,29 +40,38 @@ async fn handler(owner: &str, sender_email_sendgrid: &str, payload: EventPayload
         }
         let pull = e.pull_request;
         html_url = pull.html_url.expect("no html_url found").to_string();
-        contributor = pull.user.expect("no contributor info found").login;
+        contributor = pull.user.clone().expect("no contributor info found").login;
+        let contributor_url = pull.user.expect("no contributor info found").url.to_string();
 
         match pull.merge_commit_sha {
             Some(_) => {
-                let query_str = format!("/users/{contributor}");
-                send_message_to_channel("ik8", "general", query_str.to_string());
-                let response: OctoResult<serde_json::Value> =
-                    octocrab.get(query_str, None::<&()>).await;
-                match response {
-                    Err(_) => {}
-                    Ok(user_obj) => {
-                        contributor_email = user_obj["email"]
-                            .as_str()
-                            .unwrap_or("contributor email not found")
-                            .to_string();
-                    }
-                }
+                send_message_to_channel("ik8", "general", contributor_url.to_string());
+
+                let token = env::var("GITHUB_TOKEN").unwrap();
+
+let uri = Uri::try_from(contributor_url.as_str()).unwrap();
+
+                let mut writer = Vec::new();
+                _ = Request::new(&uri)
+                    .method(Method::GET)
+                    .header("Accept", "application/vnd.github+json")
+                    .header("User-Agent", "Github Connector of Second State Reactor")
+                    .header("Authorization", &format!("Bearer {}", token))
+                    .send(&mut writer)
+                    .map_err(|_e| {})
+                    .unwrap();
+
+                let text = String::from_utf8_lossy(&writer);
+
+                let git_user_obj: GitUser = serde_json::from_str(&text).map_err(|_e| {}).unwrap();
+                contributor_email = git_user_obj.email;
             }
             None => {
                 return;
             }
         };
     }
+
     send_message_to_channel("ik8", "general", contributor_email.to_string());
 
     let text = format!(
